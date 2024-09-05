86dff052835c0af4ebfc3b29d96c25df254d82bb
ajith-kumar-a <ajith619006@gmail.com>
2024-09-05 20:19:11 +0530

    api inter

--- Front End/AttendanceUI/src/app/app.module.ts
+++ Front End/AttendanceUI/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
- // Import FormsModule here
+import { FormsModule } from '@angular/forms'; // Import FormsModule here
 import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -10,33 +9,33 @@ import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { TokenInterceptorService } from './services/token-interceptor.service';
 import { LoginComponent } from './components/login/login.component';
 import { StudentsComponent } from './components/students/students.component';
-import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
+// import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 import { TeacherDashboardComponent } from './teacher-dashboard/teacher-dashboard.component';
 import { PersonalInfoComponent } from './personal-info/personal-info.component';
 import { ProfessionalInfoComponent } from './professional-info/professional-info.component';
 import { AttendanceMarkingComponent } from './attendance-marking/attendance-marking.component';
 import { AttendanceSummaryComponent } from './attendance-summary/attendance-summary.component';
-
+import { HttpClientModule } from '@angular/common/http'; // Import HttpClientModule
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     StudentsComponent,
-
     TeacherDashboardComponent,
     PersonalInfoComponent,
     ProfessionalInfoComponent,
     AttendanceMarkingComponent,
     AttendanceSummaryComponent
-
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbNavModule
+    // NgbNavModule,
     FormsModule,
-    CommonModule
+    CommonModule,
+    HttpClientModule // Add HttpClientModule here
+
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptorService, multi: true }
