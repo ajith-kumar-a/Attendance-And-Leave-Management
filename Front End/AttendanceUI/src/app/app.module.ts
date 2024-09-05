809ee864d77928b4dfce40a80e33bcefd9e20f78
ajith-kumar-a <ajith619006@gmail.com>
2024-09-05 07:10:49 +0530

    log in

--- Front End/AttendanceUI/src/app/app.module.ts
+++ Front End/AttendanceUI/src/app/app.module.ts
@@ -1,23 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; // Import FormsModule here
+import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { AuthInterceptor } from './interceptors/auth.interceptor';
+import { TokenInterceptorService } from './services/token-interceptor.service';
 import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    CommonModule
   ],
   providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
+    { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptorService, multi: true }
   ],
   bootstrap: [AppComponent]
 })
--- Front End/AttendanceUI/src/app/services/auth.service.ts
+++ Front End/AttendanceUI/src/app/services/auth.service.ts
@@ -1,39 +1,32 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-  private apiUrl = 'http://localhost:8000/api';
-  private tokenSubject = new BehaviorSubject<string | null>(null);
+  private apiUrl = 'http://your-django-api-url/api/';
 
   constructor(private http: HttpClient) {}
 
-  login(username: string, password: string): Observable<any> {
-    return this.http.post<any>(`${this.apiUrl}/token/`, { username, password })
-      .pipe(
-        tap(response => {
-          localStorage.setItem('access_token', response.access);
-          localStorage.setItem('refresh_token', response.refresh);
-          this.tokenSubject.next(response.access);
-        })
-      );
+  login(credentials: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}token/`, credentials);
+  }
+
+  setToken(token: string): void {
+    localStorage.setItem('token', token);
   }
 
   getToken(): string | null {
-    return localStorage.getItem('access_token');
-  }
-
-  isLoggedIn(): boolean {
-    return !!this.getToken();
+    return localStorage.getItem('token');
   }
 
   logout(): void {
-    localStorage.removeItem('access_token');
-    localStorage.removeItem('refresh_token');
-    this.tokenSubject.next(null);
+    localStorage.removeItem('token');
+  }
+
+  isAuthenticated(): boolean {
+    return !!this.getToken();
   }
 }
