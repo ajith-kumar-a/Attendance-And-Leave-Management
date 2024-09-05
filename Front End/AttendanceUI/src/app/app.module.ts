7820ff22484ebcd21e3ae48ec4a02d57e2634228
gokul-100 <gokulakanna1006@gmail.com>
2024-09-05 08:05:43 +0530

    'updated'

--- Front End/AttendanceUI/src/app/app.module.ts
+++ Front End/AttendanceUI/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { FormsModule } from '@angular/forms'; // Import FormsModule here
+ // Import FormsModule here
 import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
