254feb18a0018faf88082d305455e15a5a9a856e
mmprasanth17 <prasanthmani1711@gmail.com>
2024-09-06 10:44:37 +0530

    'prasanth'

--- Front End/AttendanceUI/src/app/components/attendance-summary/attendance-summary.component.ts
+++ Front End/AttendanceUI/src/app/components/attendance-summary/attendance-summary.component.ts
@@ -1,15 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AttendanceService } from './attendance.service';
+
+interface Attendance {
+  id: number;
+  date: string;
+  leaveType: string;
+  remark: string;
+}
 
 @Component({
   selector: 'app-attendance-summary',
   templateUrl: './attendance-summary.component.html',
-  styleUrl: './attendance-summary.component.css'
+  styleUrls: ['./attendance-summary.component.css']
 })
-export class AttendanceSummaryComponent {
-  selectedPeriod = 'Daily';
+export class AttendanceSummaryComponent implements OnInit {
+  attendanceData: Attendance[] = [];
+  filteredAttendance: Attendance[] = [];
+  startDate: string = '';
+  endDate: string = '';
 
-  generateAbsenteeReport() {
-    console.log('Generating absentee report for', this.selectedPeriod);
+  constructor(private attendanceService: AttendanceService) {}
+
+  ngOnInit(): void {
+    this.attendanceService.getAttendance().subscribe((data) => {
+      this.attendanceData = data;
+    });
   }
 
+  applyFilter(): void {
+    if (this.startDate && this.endDate) {
+      const start = new Date(this.startDate).getTime();
+      const end = new Date(this.endDate).getTime();
+
+      this.filteredAttendance = this.attendanceData.filter(item => {
+        const itemDate = new Date(item.date).getTime();
+        return itemDate >= start && itemDate <= end;
+      });
+    }
+  }
 }
