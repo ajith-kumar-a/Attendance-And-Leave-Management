e14433f43828bb52248bbfd96d4c08670c833225
mmprasanth17 <prasanthmani1711@gmail.com>
2024-09-16 11:01:11 +0530

    new added

--- Front End/AttendanceUI/src/app/components/contact-us/contact-us.component.ts
+++ Front End/AttendanceUI/src/app/components/contact-us/contact-us.component.ts
@@ -1,10 +1,29 @@
+// contact-us.component.ts
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-us',
   templateUrl: './contact-us.component.html',
-  styleUrl: './contact-us.component.css'
+  styleUrls: ['./contact-us.component.css'],
 })
 export class ContactUsComponent {
+  contactForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', [Validators.required, Validators.pattern(/^\d{10}$/)]],
+      subject: ['', Validators.required],
+      message: ['', [Validators.required, Validators.minLength(10)]],
+    });
+  }
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      console.log(this.contactForm.value);
+      // You can send the data to your backend here.
+    }
+  }
 }
